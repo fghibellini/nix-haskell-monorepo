b76fea4d9d63c8f4a0355a85443f61de60b0be1b
Filippo Ghibellini <f.ghibellini@gmail.com>
2019-05-06 17:09:56 +0200

    Fix monorepo.nix not to copy source into store.

--- shell.nix/monorepo/nix/monorepo.nix
+++ shell.nix/monorepo/nix/monorepo.nix
@@ -5,7 +5,7 @@ in
   nixpkgs.haskellPackages.mkDerivation {
     pname = "monorepo";
     version = "1.0.0";
-    src = ../code;
+    src = null;
     libraryHaskellDepends = map (pkgName: builtins.getAttr pkgName nixpkgs.haskellPackages) all-deps;
     license = nixpkgs.stdenv.lib.licenses.unfree;
   }
