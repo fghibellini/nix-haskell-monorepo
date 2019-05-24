8fc7626e5309ba94b9069bafe714b983c94874fa
Filippo Ghibellini <f.ghibellini@gmail.com>
2019-05-24 11:37:55 +0200

    Fix callCabal2nix call by switching to GHC 8.6.4

--- monorepo-nix-expressions/monorepo/nix/release.nix
+++ monorepo-nix-expressions/monorepo/nix/release.nix
@@ -7,7 +7,7 @@ let
   config = {
     allowUnfree = true;
     packageOverrides = pkgs: rec {
-      haskellPackages = pkgs.haskell.packages.ghc844.override {
+      haskellPackages = pkgs.haskell.packages.ghc864.override {
         overrides = self: super: builtins.mapAttrs (name: path: super.callCabal2nix name (gitignore path) {}) (import ./packages.nix);
       };
     };
